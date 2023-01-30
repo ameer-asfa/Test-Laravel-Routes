d4fdba568d8c6b6f068b931cb28c34a3d482e6ea
Ameer <ameer_asfa98@hotmail.com>
2023-01-30 11:18:33 +0800

    Task 10

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::middleware('auth')->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
+        Route::redirect('/dashboard', DashboardController::class);
 
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
